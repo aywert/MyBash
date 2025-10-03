10463ab5aa5bfdd39d21337baf7a25dbdbcf5d84
Michael Movsesian <michaelm0vsesyan@yandex.ru>
2025-10-03 23:06:52 +0300

    working version without redirections

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include "io.h"
 
-int init_processes(char* GlobalArgv);
+int init_processes(size_t Num);
 char** get_argv_per_process(char* sample_argv, size_t length);
 int get_bash_argv(void);
 
 char GlobalArgv[4096];
+char* Operations[255];
  
 int main(void)
 {
   int run = 1; 
   while(run)
   {
-    printf("bash: ");
+    //printf("bash: ");
+    system("echo -n $USER@~: ");
+    fflush(stdout);
 
-    get_bash_argv();
+    size_t Num = get_bash_argv();
 
-    init_processes(GlobalArgv);
-    
-    run = 0;
+    init_processes(Num);
+
+    //run = 0;
   }
 
   return 0;
@@ -32,27 +36,50 @@ int get_bash_argv(void)
     GlobalArgv[i] = ch; i++;
   }
 
-  return 0;
+  size_t index = 0;
+  char* sample_argv = strtok(GlobalArgv, "|");
+  
+  while(sample_argv!= NULL) {
+    Operations[index++] = sample_argv;
+    sample_argv = strtok(NULL, "|"); 
+  }
+
+  return index;
 }
 
-int init_processes(char* GlobalArgv)
+int init_processes(size_t Num)
 {
-  char* sample_argv = strtok(GlobalArgv, "|");
+  int input_fd = STDIN_FILENO; // first process reading from stdin
+  
+  for(size_t index = 0; index < Num; index++){
 
-  while(sample_argv!= NULL) {
-    //printf("it is me: '%s'\n", sample_argv);
+    int fds[2];
+    pipe(fds); // fds[0] - reading end | fds[1] = writing end 
+
     pid_t pid = fork();
-    if (pid != 0)
+    if (pid == 0)
     {
-      char** argv_array = get_argv_per_process(sample_argv, strlen(sample_argv));
+      if (index < Num - 1)
+        dup2(fds[1], 1); //writing in output means writing in write end of the pipe
+
+      dup2(input_fd, 0); //reading from input_fd(at the begging it equals to stdin, then to reading end of pipe)
+      char** argv_array = get_argv_per_process(Operations[index], strlen(Operations[index]));
       execvp(argv_array[0], argv_array);  
       //if execvp returned it means that error occured and we need to clean after execvp 
       perror("execvp failed");
+      //close(new_fd);
       free(argv_array); argv_array = NULL;
-      return 0;
+      exit(0);
     } 
-    
-    sample_argv = strtok(NULL, "|"); 
+
+    //int new_fd = dup2(fds[0], 0); //reading from stdin means reading from reading end of the pipe
+    close(fds[1]); //closing writing end of pipe
+    input_fd = fds[0]; // reading end of previous pipe;
+  }
+
+  int status;
+  for (size_t i = 0; i < Num; i++) {
+    wait(&status);
   }
 
   return 0;
